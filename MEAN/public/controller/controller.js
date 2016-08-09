eabfc91a42fbf8a2623ab999634d57bed0955924
Qiqiang Guan <qiqiang.guan@outlook.com>
2016-08-08 22:22:59 -0700

    add controller

--- MEAN/public/controller/controller.js
+++ MEAN/public/controller/controller.js
@@ -2,5 +2,17 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http',
     function($scope, $http) {
         console.log("Hello world from controller!");
+
+        person1 = {
+            name = 'Tim',
+            email='tim@email.com',
+            number='(111) 111-1111'
+        };
+
+        person2 = {
+            name = 'Tim',
+            email='tim@email.com',
+            number='(111) 111-1111'
+        };
     }
 ]);
