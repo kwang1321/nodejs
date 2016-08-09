d173b848988044fa473f84bcd3b5ed4a96bf0bc7
Qiqiang Guan <qiqiang.guan@outlook.com>
2016-08-09 14:09:41 -0700

    MEAN display example

--- MEAN/public/controller/controller.js
+++ MEAN/public/controller/controller.js
@@ -4,15 +4,24 @@ myApp.controller('AppCtrl', ['$scope', '$http',
         console.log("Hello world from controller!");
 
         person1 = {
-            name = 'Tim',
-            email='tim@email.com',
-            number='(111) 111-1111'
+            name: 'Tim',
+            email: 'tim@email.com',
+            number: '(111) 111-1111'
         };
 
         person2 = {
-            name = 'Tim',
-            email='tim@email.com',
-            number='(111) 111-1111'
+            name: 'Emily',
+            email: 'emily@email2.com',
+            number: '(222) 222-2222'
         };
+
+        person3 = {
+            name: 'John',
+            email: 'john@email3.com',
+            number: '(333) 333-3333'
+        };
+
+        var contactList = [person1, person2, person3];
+        $scope.contactList = contactList;
     }
 ]);
