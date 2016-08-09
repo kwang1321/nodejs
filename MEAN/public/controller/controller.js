5ab2fd5f28ce73b8764f078d1fc4ec3ce65dd71a
Qiqiang Guan <qiqiang.guan@outlook.com>
2016-08-09 14:25:23 -0700

    Angular JS and Node JS App Tutorial

--- MEAN/public/controller/controller.js
+++ MEAN/public/controller/controller.js
@@ -3,25 +3,10 @@ myApp.controller('AppCtrl', ['$scope', '$http',
     function($scope, $http) {
         console.log("Hello world from controller!");
 
-        person1 = {
-            name: 'Tim',
-            email: 'tim@email.com',
-            number: '(111) 111-1111'
-        };
-
-        person2 = {
-            name: 'Emily',
-            email: 'emily@email2.com',
-            number: '(222) 222-2222'
-        };
-
-        person3 = {
-            name: 'John',
-            email: 'john@email3.com',
-            number: '(333) 333-3333'
-        };
-
-        var contactList = [person1, person2, person3];
-        $scope.contactList = contactList;
+        $http.get("/contactlist").success(function(response) {
+            console.log("get data from server side");
+            console.log(response);
+            $scope.contactList = response;
+        });
     }
 ]);
--- MEAN/server.js
+++ MEAN/server.js
@@ -8,5 +8,30 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get("/contactlist", function(req, res) {
+    console.log("this is a get request");
+    person1 = {
+        name: 'Tim',
+        email: 'tim@email.com',
+        number: '(111) 111-1111'
+    };
+
+    person2 = {
+        name: 'Emily',
+        email: 'emily@email2.com',
+        number: '(222) 222-2222'
+    };
+
+    person3 = {
+        name: 'John',
+        email: 'john@email3.com',
+        number: '(333) 333-3333'
+    };
+
+    var contactList = [person1, person2, person3];
+    res.json(contactList);
+
+});
+
 app.listen(3000);
 console.log("Server is running on port 3000");
