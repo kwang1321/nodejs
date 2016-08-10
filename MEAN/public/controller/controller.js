122356c028be03204ade0fd03d488c089cfe374f
gqq w541 ubuntu <gqq_w541_ubuntu@itu.edu>
2016-08-09 23:52:14 -0700

    RESTful API, GET and POST Data

--- MEAN/public/controller/controller.js
+++ MEAN/public/controller/controller.js
@@ -3,10 +3,30 @@ myApp.controller('AppCtrl', ['$scope', '$http',
     function($scope, $http) {
         console.log("Hello world from controller!");
 
-        $http.get("/contactlist").success(function(response) {
-            console.log("get data from server side");
-            console.log(response);
-            $scope.contactList = response;
-        });
+
+        var refresh = function() {
+            $http.get("/contactlist").success(function(response) {
+
+                console.log("get data from server side");
+                console.log(response);
+                $scope.contactList = response;
+                $scope.contact = {
+                    name: "",
+                    email: "",
+                    number: ""
+                };
+            });
+        };
+
+        refresh();
+
+        $scope.addcontact = function() {
+            console.log($scope.contact);
+            $http.post("/contactlist", $scope.contact).success(function(response) {
+                console.log(response);
+                // $scope.contactList.push(response);
+                refresh();
+            });
+        }
     }
 ]);
---
+++ MEAN/server2.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+
+var mongojs = require("mongojs");
+var db = mongojs("contactlist", ['contactlist']);
+// app.get('/', function(req, res) {
+//     res.send('Hello world from sever.js');
+// })
+//
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/contactlist", function(req, res) {
+    console.log("this is a get request");
+//    person1 = {
+//        name: 'Tim',
+//        email: 'tim@email.com',
+//        number: '(111) 111-1111'
+//    };
+//
+//    person2 = {
+//        name: 'Emily',
+//        email: 'emily@email2.com',
+//        number: '(222) 222-2222'
+//    };
+//
+//    person3 = {
+//        name: 'John',
+//        email: 'john@email3.com',
+//        number: '(333) 333-3333'
+//    };
+//
+//    var contactList = [person1, person2, person3];
+//    res.json(contactList);
+    db.contactlist.find(function(err, docs){
+        console.log(docs);
+        res.json(docs);
+    })
+
+
+});
+
+app.listen(3000);
+console.log("Server is running on port 3000");
